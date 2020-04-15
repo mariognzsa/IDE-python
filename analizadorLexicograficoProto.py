d9cbae04d806dd10809e7e511cafc5eeb6d585c3
Mario González Santoyo <mario_gnzsa@hotmail.com>
2020-04-15 15:58:32 -0500

    Complete cases for the token recognition

--- analizadorLexicograficoProto.py
+++ analizadorLexicograficoProto.py
@@ -2,6 +2,15 @@
 
 import sys, getopt
 
+class Token:
+    def __init__(self, id, tokenType, token, start, end):
+        self.id = id
+        self.tokenType = tokenType
+        self.token = token
+        self.start = start
+        self.end = end
+
+
 def main(argv):
     #filename = argv[0]
     #new_file = open(filename, 'r')
@@ -9,10 +18,20 @@ def main(argv):
     #new_file.close()
     
     # Hardcoded input if empty
-    if argv[0] == '':
-        inputCode = 'if else jajaja /*** esto es* jajaja **/ //comentariooooooo\n aqui valio dick 123.123 12 123..123..2'
-    else:
-        inputCode = argv[0]
+    
+    inputCode = """
+    int main ( int number, char codeX ){
+        if 5 >= 3 and 4 < 5
+            cout << number * 5
+            // one line comment
+            /* here
+                we test the 
+                multiline
+            */
+        float floatNumber = 4.3
+    }
+    
+    """
     print(inputCode)
     state = 0
     restricted = [  # List of restricted words
@@ -27,13 +46,19 @@ def main(argv):
         'cout', 
         'real', 
         'int', 
-        'boolean'
+        'boolean',
+        'float',
+        'char'
     ]
     charset = 'abcdefghijklmnopqrstuvwxyz' # array permitted for identifier's first character
     charsetExtended = 'abcdefghijklmnopqrstuvwxyz_1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     numbers = '0123456789'
-    tokens = []  # Array of founded tokens
+    operators = '+-<>=|!/*'
+    spacesAndStuff = ' \n'  # Pls rename later
+    tokens = []  # Array of found tokens
     currentToken = ''
+    tokenStart = 0
+    tokenEnd = 0
     cIndex = 0   # index for current character
     tkIndex = 0 #  index for the current token
     while cIndex < len(inputCode):
@@ -42,31 +67,42 @@ def main(argv):
         if state == 0:
             if inputCode[cIndex] in charset: # Checking for identifier (frst letter lowercase)
                 currentToken += inputCode[cIndex]
+                tokenStart = cIndex
                 cIndex += 1
                 while inputCode[cIndex] in charsetExtended: # Checking on extended charset w / uppercase and numbers
                     currentToken += inputCode[cIndex]
                     cIndex += 1
                 tokenType = 'identifier'
+                tokenEnd = cIndex
 
             elif inputCode[cIndex] in numbers: # Checking for number appearances
                 currentToken += inputCode[cIndex]
+                tokenStart = cIndex
                 cIndex += 1
                 number = True
+                floatingNumber = False
                 decimalPoints = 0
                 while number and decimalPoints <= 1:  # Loop just for numbers and one decimalpoint max
                     if inputCode[cIndex] in numbers:
                         currentToken += inputCode[cIndex]
                         cIndex += 1
                     elif inputCode[cIndex] == '.':
-                        if decimalPoints < 1 and inputCode[cIndex+1] in numbers:
-                            currentToken += inputCode[cIndex]                            
+                        if decimalPoints < 1 and inputCode[cIndex+1] in numbers: # Turns on a flag to differenciate int and float
+                            currentToken += inputCode[cIndex]
+                            floatingNumber = True                          
                         cIndex += 1
                         decimalPoints += 1
+                        
                     else:
                         number = False
-                tokenType = 'digit'
+                if floatingNumber:
+                    tokenType = 'float'
+                else:
+                    tokenType = 'integer'
+                tokenEnd = cIndex
 
-            elif inputCode[cIndex] == '/': # Checking for commentary lines
+            elif inputCode[cIndex] == '/' and (inputCode[cIndex+1] == '/' or inputCode[cIndex+1] == '*'): # Checking for commentary lines
+                tokenStart = cIndex
                 cIndex += 1
                 if inputCode[cIndex] == '/': # case // for one line comments
                     cIndex += 1
@@ -90,11 +126,62 @@ def main(argv):
                                 cIndex += 1
 
                     tokenType = 'multiline_commentary'
+                tokenEnd = cIndex
+
+            elif inputCode[cIndex] in operators: # Checking if is an operator, btw unnecessary cases lol
+                tokenStart = cIndex
+                if inputCode[cIndex] == '<':
+                    currentToken += inputCode[cIndex]
+                    cIndex += 1
+                    if inputCode[cIndex] == '=':
+                        currentToken += inputCode[cIndex]
+                        cIndex += 1
+                elif inputCode[cIndex] == '>':
+                    currentToken += inputCode[cIndex]
+                    cIndex += 1
+                    if inputCode[cIndex] == '=':
+                        currentToken += inputCode[cIndex]
+                        cIndex += 1
+                elif inputCode[cIndex] == '=':
+                    currentToken += inputCode[cIndex]
+                    cIndex += 1
+                    if inputCode[cIndex] == '=':
+                        currentToken += inputCode[cIndex]
+                        cIndex += 1
+                elif inputCode[cIndex] == '!':
+                    currentToken += inputCode[cIndex]
+                    cIndex += 1
+                    if inputCode[cIndex] == '=':
+                        currentToken += inputCode[cIndex]
+                        cIndex += 1
+                elif inputCode[cIndex] == '+':
+                    currentToken += inputCode[cIndex]
+                    cIndex += 1
+                    if inputCode[cIndex] == '=' or inputCode[cIndex] == '+': # special cases for ++ and +=, considering them as operators
+                        currentToken += inputCode[cIndex]
+                        cIndex += 1
+                elif inputCode[cIndex] == '-':
+                    currentToken += inputCode[cIndex]
+                    cIndex += 1
+                    if inputCode[cIndex] == '=' or inputCode[cIndex] == '-':
+                        currentToken += inputCode[cIndex]
+                        cIndex += 1
+                else:                                       # if not one of above, no need in checking for further chars
+                    currentToken += inputCode[cIndex]
+                tokenType = 'operator'
+                tokenEnd = cIndex
+                
+            elif inputCode[cIndex] not in charsetExtended and inputCode[cIndex] not in numbers and inputCode[cIndex] not in operators and inputCode[cIndex] not in spacesAndStuff:
+                # Considered special character if not in the above alphabets
+                tokenStart = cIndex
+                tokenEnd = cIndex+1
+                currentToken += inputCode[cIndex]
+                tokenType = 'special_character'
 
             if len(currentToken) > 0 and tokenType != '': # Checking token and tokentype not empty
                 if currentToken in restricted: # If the token content is a restricted word we manage it
                     tokenType = 'restricted_word'
-                tokenInfo = [tkIndex, currentToken, tokenType]
+                tokenInfo = Token(tkIndex, tokenType, currentToken, tokenStart, tokenEnd)
                 tokens.append(tokenInfo)
                 tokenType = ''
                 currentToken = ''
@@ -102,7 +189,7 @@ def main(argv):
         cIndex += 1    
             
     for token in tokens:
-        print(str(token))
+        print('id: '+str(token.id)+'|| type: '+str(token.tokenType)+'|| token: '+str(token.token)+'|| start: '+str(token.start)+'|| end: '+str(token.end))
 
     
 if __name__ == "__main__":
